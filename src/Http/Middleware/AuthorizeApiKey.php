259260fefde8a284a653c5e649737441f2d5f203
Jevgenijs Fokins <jf@whaleslide.com>
2018-05-28 01:40:06 +0300

    accept key from body as a 2nd option

--- src/Http/Middleware/AuthorizeApiKey.php
+++ src/Http/Middleware/AuthorizeApiKey.php
@@ -20,7 +20,7 @@ class AuthorizeApiKey
      */
     public function handle(Request $request, Closure $next)
     {
-        $header = $request->header(self::AUTH_HEADER);
+        $header = $request->header(self::AUTH_HEADER) ?? $request->key;
         $apiKey = ApiKey::getByKey($header);
 
         if ($apiKey instanceof ApiKey) {
